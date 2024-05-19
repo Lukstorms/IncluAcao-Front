9e876b65bc889c48a935883ba454fdfef3a53f6a
Lucas Oliveira Viana <94014784+Lukstorms@users.noreply.github.com>
2024-05-19 12:20:57 -0300

    agora vai

---
+++ js/firebase-init.js
@@ -0,0 +1,14 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDfSd3zbl4iOT_Dw3RA0nk4HVo1SrUcCsc",
+    authDomain: "incluacao-fb776.firebaseapp.com",
+    projectId: "incluacao-fb776",
+    storageBucket: "incluacao-fb776.appspot.com",
+    messagingSenderId: "198502388108",
+    appId: "1:198502388108:web:bd387b9504810af7d586a9"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  
