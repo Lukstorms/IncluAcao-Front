fd1dffe26a527fbb004a6c29a5f436756272d9bd
Lucas Oliveira Viana <luks.viana@gmail.com>
2024-05-27 16:27:43 -0300

    vercel env

--- js/firebaseConfig.mjs
+++ js/firebaseConfig.mjs
@@ -2,14 +2,14 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 
-// Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDfSd3zbl4iOT_Dw3RA0nk4HVo1SrUcCsc",
-  authDomain: "incluacao-fb776.firebaseapp.com",
-  projectId: "incluacao-fb776",
-  storageBucket: "incluacao-fb776.appspot.com",
-  messagingSenderId: "198502388108",
-  appId: "1:198502388108:web:bd387b9504810af7d586a9"
+  apiKey: import.meta.env.VERCEL_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VERCEL_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VERCEL_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VERCEL_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VERCEL_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VERCEL_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
