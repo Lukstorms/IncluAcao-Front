9cb313ebff7010729c9807316021d4fb737de9c0
Lucas Oliveira Viana <luks.viana@gmail.com>
2024-05-28 10:12:01 -0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv()
+  ],
+  resolve: {
+    extensions: ['.js', '.mjs']
+  }
+};
